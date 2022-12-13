95a6980185dffec79ff5b65380aaade4fb40d17f
pejowalker <106663915+pejowalker@users.noreply.github.com>
2022-12-13 00:29:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
 
-function increment() {
-    count += 1
-    countEl.textContent = count
-}
+const increment-btn = document.getElementById("increment-btn");
+increment-btn.addEventListener("click", function() {
+	 count += 1
+     countEl.textContent = count
+}, false);
 
-function save() {
-    let countStr = count + " - "
+const save-btn = document.getElementById("save-btn");
+save-btn.addEventListener("click", function() {
+	let countStr = count + " - "
     saveEl.textContent += countStr
     countEl.textContent = 0
     count = 0
-}
+}, false);
